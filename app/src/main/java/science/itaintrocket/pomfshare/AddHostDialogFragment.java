c826d5664016c8b5f58c7e54e745d694e080d8a2
flan <flan@flande.re>
2017-03-14 17:32:52 +0100

    Add basic error handling to the add dialog

--- app/src/main/java/science/itaintrocket/pomfshare/AddHostDialogFragment.java
+++ app/src/main/java/science/itaintrocket/pomfshare/AddHostDialogFragment.java
@@ -9,8 +9,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class AddHostDialogFragment extends DialogFragment {
 	@Override
@@ -22,41 +25,7 @@ public class AddHostDialogFragment extends DialogFragment {
 		builder.setMessage("If you fill this out wrong the app will probably crash")
 				.setTitle("Add Host")
 				.setView(inflater.inflate(R.layout.dialog_add_host, null))
-				.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						HostDbHelper helper = new HostDbHelper(getActivity().getApplicationContext());
-						SQLiteDatabase db = helper.getWritableDatabase();
-						ContentValues values = new ContentValues();
-
-						EditText nameEdit = (EditText)((AlertDialog)dialog).findViewById(R.id.host_name);
-						EditText urlEdit = (EditText)((AlertDialog)dialog).findViewById(R.id.host_url);
-						EditText descEdit = (EditText)((AlertDialog)dialog).findViewById(R.id.host_description);
-						RadioGroup typeRG = (RadioGroup)((AlertDialog)dialog).findViewById(R.id.host_api_type);
-
-						Host.Type type = null;
-
-						switch(typeRG.getCheckedRadioButtonId()) {
-							case R.id.pomf_api:
-								type = Host.Type.POMF;
-								break;
-							case R.id.uguu_api:
-								type = Host.Type.UGUU;
-								break;
-						}
-
-						if(type == null) {
-							Log.e("ayy lmao", "no host type selected rip");
-							// TODO: show some error
-						}
-
-						values.put(HostDbContract.HostTable.COL_HOST_NAME, nameEdit.getText().toString());
-						values.put(HostDbContract.HostTable.COL_HOST_URL, urlEdit.getText().toString());
-						values.put(HostDbContract.HostTable.COL_HOST_DESC, descEdit.getText().toString());
-						values.put(HostDbContract.HostTable.COL_HOST_API, type.name());
-
-						db.insert(HostDbContract.HostTable.TABLE_NAME, null, values);
-					}
-				})
+				.setPositiveButton("Add", null)
 				.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int id) {
 						// This entire method can maybe be removed?
@@ -64,5 +33,54 @@ public class AddHostDialogFragment extends DialogFragment {
 				});
 		// Create the AlertDialog object and return it
 		return builder.create();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		final AlertDialog d = (AlertDialog)getDialog();
+		Button addButton = d.getButton(Dialog.BUTTON_POSITIVE);
+		addButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				HostDbHelper helper = new HostDbHelper(getActivity().getApplicationContext());
+				SQLiteDatabase db = helper.getWritableDatabase();
+				ContentValues values = new ContentValues();
+
+				EditText nameEdit = (EditText)d.findViewById(R.id.host_name);
+				EditText urlEdit = (EditText)d.findViewById(R.id.host_url);
+				EditText descEdit = (EditText)d.findViewById(R.id.host_description);
+				RadioGroup typeRG = (RadioGroup)d.findViewById(R.id.host_api_type);
+
+				Host.Type type = null;
+
+				switch(typeRG.getCheckedRadioButtonId()) {
+					case R.id.pomf_api:
+						type = Host.Type.POMF;
+						break;
+					case R.id.uguu_api:
+						type = Host.Type.UGUU;
+						break;
+				}
+
+				if(urlEdit.getText().length() == 0) {
+					urlEdit.setError("URL can't be empty");
+					return;
+				}
+
+				if(type == null) {
+					Toast.makeText(getActivity(), "No host type selected", Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				values.put(HostDbContract.HostTable.COL_HOST_NAME, nameEdit.getText().toString());
+				values.put(HostDbContract.HostTable.COL_HOST_URL, urlEdit.getText().toString());
+				values.put(HostDbContract.HostTable.COL_HOST_DESC, descEdit.getText().toString());
+				values.put(HostDbContract.HostTable.COL_HOST_API, type.name());
+
+				db.insert(HostDbContract.HostTable.TABLE_NAME, null, values);
+				d.dismiss();
+			}
+		});
 	}
 }
